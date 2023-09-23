61e234672521a3b6e36253518b36b75d1063effa
Timothy Olili <timmolili27@gmail.com>
2023-09-24 01:16:16 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	let first = document.getElementsByName("fname")[0];
+	console.log(first);
+	
+	let last = document.getElementsByName("lname")[0];
+	console.log(last);
 }
